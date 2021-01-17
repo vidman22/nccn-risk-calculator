8c450bc01f49c31926bb43020b14d5bd59cfa44f
vidman22 <29338715+vidman22@users.noreply.github.com>
2021-01-17 14:40:45 -0800

    lint

--- src/components/InfoModal/InfoModal.tsx
+++ src/components/InfoModal/InfoModal.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faInfoCircle, faTimes } from '@fortawesome/free-solid-svg-icons';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
 import './InfoModal.css';
 
 type Props = {
